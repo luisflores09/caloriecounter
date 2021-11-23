636cd04dbddaf82ca98de974050f24190b0a5988
Luis <iluisflores@yahoo.com>
2021-11-23 14:10:04 -0600

    "added comma to sigup path"

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('accounts/signup/', views.signup, name='signup')
+    path('accounts/signup/', views.signup, name='signup'),
 ]
