d5ab3da8b366b74f7d23f254d0ff0146dfdd6f65
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-27 15:23:39 -0500

    Added cascade to delete method

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 # Create your models here.
 
 class User(models.Model):
@@ -13,5 +13,6 @@ class Food(models.Model):
     name = models.CharField(max_length=100)
     calories = models.IntegerField()
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    on_delete=models.CASCADE
     def get_absolute_url(self):
         return reverse('index')
