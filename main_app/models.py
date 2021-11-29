13bf479e2a4f6a6f9d1d0204f1d3e58779ee12ab
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-29 14:00:08 -0500

    Removed dead code

--- main_app/models.py
+++ main_app/models.py
@@ -6,13 +6,9 @@ from django.contrib.auth.models import User
 class User(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Home(models.Model):
-    pass
-
 class Food(models.Model):
     name = models.CharField(max_length=100)
     calories = models.IntegerField()
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     on_delete=models.CASCADE
     def get_absolute_url(self):
         return reverse('index')
