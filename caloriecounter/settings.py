84e987b399b15571e27d3216e2264236efbe9c30
Luis <iluisflores@yahoo.com>
2021-12-01 11:55:05 -0600

    'updated requirements.txt'
    Co-authored-by: Ryan Scarbel <rscarbel@users.noreply.github.com>
    Co-authored-by: kelton117 <kelton117@users.noreply.github.com>
    Co-authored-by: Anthony Lee <Thethony@users.noreply.github.com>

--- caloriecounter/settings.py
+++ caloriecounter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7nif^)q$qssnlvxj*8t7j#g%dd*7zl9*=ei$0cs8aodpajlh0b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
